2f1d9d1aa6e00353b90c56c391a1c583d111a189
Vehementz <vale-tudo91@sfr.fr>
2023-03-27 11:37:41 +0200

    B2 done

--- src/challenges/B2/answer.ts
+++ src/challenges/B2/answer.ts
@@ -2,16 +2,60 @@
  * In this challenge, you have to get all the categories from the events. The categories 
  * must be unique and sorted from A to Z.
  * 
+ * 
+ * 
+ * 
  * @param events List of events with their categories
  * @returns All existing categories sorted alphabatically without duplicates (A to Z)
  */
 
 // ↓ uncomment bellow lines and add your response!
-/*
+
 export default function ({ events }: { events: EventWithCategory[] }): string[] {
-    return [];
+
+
+    let category: any = [];
+
+    category = events.map(({categories}) => ({categories}).categories)
+
+
+
+
+
+    let i = 0;
+    let j = 0;
+    let elementsCategory: any = [];
+
+    for (i = 0; i < category.length; i++) {
+        // console.log(category[i])
+        if (category[i].length > 0 ) {
+            for(j = 0; j < category[i].length; j++ ){
+                elementsCategory.push(category[i][j])
+            }
+
+        }
+    }
+
+
+
+    let filteredElementsCategory: any = [];
+    filteredElementsCategory = elementsCategory.filter(function(item, pos) {
+        return elementsCategory.indexOf(item) == pos;
+    })
+
+
+    let sortedCategory = filteredElementsCategory.sort((a, b) => a.localeCompare(b));
+      
+
+    return sortedCategory;
 }
-*/
+
+
+
+
+
+
+
 
 // used interfaces, do not touch
 export interface EventWithCategory {
--- src/challenges/B2/output.ts
+++ src/challenges/B2/output.ts
@@ -6,3 +6,4 @@ export default [
     "Quest",
     "Workshop"
 ] as string[];
+
